9102ae4397e4f48da06f08e06d7b6a646d6053e7
infinitron <karthikreddy311@gmail.com>
2024-10-17 08:46:07 +0000

    added page metadata

--- pages/waterfalls/spectrograms/dailydigest.vue
+++ pages/waterfalls/spectrograms/dailydigest.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Daily Digest Spectrograms @ LWA-Sevilleta',
+  ogTitle:  'Daily Digest Spectrograms @ LWA-Sevilleta',
+  description: 'Explore day-wise spectrograms from the Long Wavelength Array, Sevilleta',
+  ogDescription: 'Explore day-wise spectrograms from the Long Wavelength Array, Sevilleta'
+})
+</script>
 <template>
   <SpectrogramDailyDigestDisplay/>
 </template>
