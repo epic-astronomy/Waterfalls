f4b660d46cbfd1c32e0f5391749c24f766616f37
infinitron <karthikreddy311@gmail.com>
2024-10-18 07:44:45 +0000

    SEO thumbnail for digest page

--- pages/waterfalls/spectrograms/dailydigest.vue
+++ pages/waterfalls/spectrograms/dailydigest.vue
@@ -3,7 +3,10 @@ useSeoMeta({
   title: 'Daily Digest Spectrograms @ LWA-Sevilleta',
   ogTitle:  'Daily Digest Spectrograms @ LWA-Sevilleta',
   description: 'Explore day-wise spectrograms from the Long Wavelength Array, Sevilleta',
-  ogDescription: 'Explore day-wise spectrograms from the Long Wavelength Array, Sevilleta'
+  ogDescription: 'Explore day-wise spectrograms from the Long Wavelength Array, Sevilleta',
+  ogImage: 'https://epic-astronomy.org/daily-digest.png',
+  twitterImage: 'https://epic-astronomy.org/daily-digest.png',
+  twitterCard: 'summary_large_image'
 })
 </script>
 <template>
